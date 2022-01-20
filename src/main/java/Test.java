ec5bf547cfa737c3855d3c6c8c802b2167eb908c
GiangPH <giangph@unifiedpost.com>
2022-01-20 17:08:55 +0700

    update develop 1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,7 +4,7 @@ public class Test {
         System.out.println("Line 2");
         System.out.println("Line 3");
         System.out.println("Line 4");
-        System.out.println("Develop");
+        System.out.println("Develop1");
         System.out.println("Line 5");
         System.out.println("Line 6");
         System.out.println("Line 7");
