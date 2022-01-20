98feb7fc3205c4487e3e4c822128151845c18523
GiangPH <giangph@unifiedpost.com>
2022-01-20 17:14:01 +0700

    add develop 2

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,13 +4,13 @@ public class Test {
         System.out.println("Line 2");
         System.out.println("Line 3");
         System.out.println("Line 4");
-        System.out.println("Develop1");
+        System.out.println("Develop2");
         System.out.println("Line 5");
         System.out.println("Line 6");
         System.out.println("Line 7");
         System.out.println("Line 8");
         System.out.println("Line 9");
         System.out.println("Line 10");
-        System.out.println("Rebase 2");
+        System.out.println("Rebase 1");
     }
 }
