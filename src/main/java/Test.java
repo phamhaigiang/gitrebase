6a1e5d004b09099c3cd40f784fd52046737c14be
GiangPH <giangph@unifiedpost.com>
2022-01-20 19:52:10 +0700

    add dev 70

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,17 +4,17 @@ public class Test {
         System.out.println("Line 2");
         System.out.println("Line 3");
         System.out.println("Line 4");
-        System.out.println("Develop 50");
-        System.out.println("Develop 50");
-        System.out.println("Develop 50");
+        System.out.println("Develop 70");
+        System.out.println("Develop 70");
+        System.out.println("Develop 70");
         System.out.println("Line 5");
         System.out.println("Line 6");
         System.out.println("Line 7");
         System.out.println("Line 8");
         System.out.println("Line 9");
         System.out.println("Line 10");
-        System.out.println("Rebase 20");
-        System.out.println("Rebase 20");
-        System.out.println("Rebase 50");
+        System.out.println("Rebase 10");
+        System.out.println("Rebase 10");
+        System.out.println("Rebase 10");
     }
 }
